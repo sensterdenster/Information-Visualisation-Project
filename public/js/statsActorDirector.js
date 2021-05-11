3c7640d7404c3698d6bbd2b40ad46b0b269b543a
Sen Thotawatte <sen.thotawatte@gmail.com>
2021-05-11 20:07:21 +0100

    additional changes

--- public/js/statsActorDirector.js
+++ public/js/statsActorDirector.js
@@ -12,67 +12,66 @@ class StatsActorDirector{
     {
         let statsActorDirector_Div = d3.select("#statsActorDirector");
 
-        let margin = {top: 19, right: 19, bottom: 99, left: 119},
-        svgBounds = statsActorDirector_Div.node().getBoundingClientRect(),
-        height = 449 - margin.top - margin.bottom,
-        width = svgBounds.width - margin.right - margin.left;
-
         let svg = d3.select("#plotTrend")
-        .attr("height", 449 + margin.bottom + margin.top)
-        .attr("width", svgBounds.width);
+            .attr("height", 449 + margin.bottom + margin.top)
+            .attr("width", svgBounds.width);
+
+        let margin = {top: 19, right: 19, bottom: 99, left: 119},
+            svgBounds = statsActorDirector_Div.node().getBoundingClientRect(),
+            height = 449 - margin.top - margin.bottom,
+            width = svgBounds.width - margin.right - margin.left;
+
+        let scaleX = d3.scaleBand()
+            .rangeRound([0, width])
+            .padding([1])
+            .domain((this.films).map(d => d["movie_title"]));
+
+        let scaleY = d3.scaleLinear()
+            .range([height, 0])
+            .domain(d3.extent(this.films, (d) => { return parseFloat(d[this.feature])}));
 
         let ptg = d3.select("#plotTrendGroup")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-        let xScale = d3.scaleBand()
-        .rangeRound([0, width])
-        .padding([1])
-        .domain((this.films).map(d => d["movie_title"]));
-
-        let yScale = d3.scaleLinear()
-        .range([height, 0])
-        .domain(d3.extent(this.films, (d) => { return parseFloat(d[this.feature])}));
-
-        yScale.nice();
+        scaleY.nice();
 
         //Using d3 to add y-axis
         d3.select("#yAxis")
-            .transition()
             .duration(1500)
-            .call(d3.axisLeft(yScale));
+            .transition()
+            .call(d3.axisLeft(scaleY));
 
         //Y-axis label being added
-        let yLabel = d3.select("#yLabel").selectAll("text")
+        let Labely = d3.select("#yLabel").selectAll("text")
             .data([this.feature]);
 
         //Customising the y-axis label font, color, position, opacity, etc
-        let yLabelEnter = yLabel.enter().append("text");
-        yLabel.exit().remove();
-        yLabel = yLabel.merge(yLabelEnter)
-            .attr("class", "font-weight-bold text-capitalize")
-            .attr("fill", "#000")
-            .style("opacity", 0)
+        let EnterLabely = Labely.enter().append("text");
+        Labely.exit().remove();
+        Labely = Labely.merge(EnterLabely)
+            .text((d) => { return d; })
             .attr("transform", "rotate(-90)")
             .attr("x", -height/2)
             .attr("y", -width/10)
             .attr("text-anchor", "middle")
-            .text((d) => { return d; })
-            .transition()
+            .attr("fill", "#000")
+            .attr("class", "font-weight-bold text-capitalize")
             .duration(1500)
-            .style("opacity", 1);
+            .style("opacity", 0)
+            .style("opacity", 1)
+            .transition();
 
         //Add the x Axis
         d3.select("#xAxis")
-            .attr("transform", "translate(" + 0 + "," + height + ")")
-            .transition()
-            .duration(1500)
-            .call(d3.axisBottom(xScale))
+            .call(d3.axisBottom(scaleX))
             .selectAll("text")
-            .style("text-anchor", "end")
             .attr("dx", "-.8em")
             .attr("dy", ".15em")
-            .attr("transform", "rotate(-65)");
-
+            .attr("transform", "translate(" + 0 + "," + height + ")")
+            .attr("transform", "rotate(-65)")
+            .duration(1500)
+            .style("text-anchor", "end")
+            .transition();
 
         let textLabelx = (this.directorOrActor) + " " + this.nameDirectorActor + "'s" + " films";
 
@@ -84,16 +83,16 @@ class StatsActorDirector{
         let EnterLabelx = Labelx.enter().append("text");
         Labelx.exit().remove();
         Labelx = Labelx.merge(EnterLabelx)
-            .attr("class", "font-weight-bold")
+            .text((d) => { return d; })
             .attr("fill", "#000")
-            .style("opacity", 0)
+            .attr("class", "font-weight-bold")
             .attr("x", width/2)
             .attr("y", -7)
-            .text((d) => { return d; })
             .attr("text-anchor", "middle")
-            .transition()
             .duration(1500)
-            .style("opacity", 1);
+            .style("opacity", 0)
+            .style("opacity", 1)
+            .transition();
 
         //Plotpoints being added 
         let plotPoints = ptg.selectAll("circle")
@@ -113,21 +112,21 @@ class StatsActorDirector{
         plotPoints = plotPoints.merge(enterPoints);
 
         plotPoints
-            .transition()
             .duration(1500)
-            .attr("r", 4.5)
-            .attr("cx", (d) => { return xScale(d["movie_title"]); })
-            .attr("cy", (d) => { return yScale(d[this.feature]); });
+            .transition()
+            .attr("cy", (d) => { return scaleY(d[this.feature]); })
+            .attr("cx", (d) => { return scaleX(d["movie_title"]); })
+            .attr("r", 4.5);
 
         //Invoke the tip on the plot points
         plotPoints.call(tip)
-            .on("mouseover", tip.show)
-            .on("mouseout", tip.hide);
+            .on("mouseout", tip.hide)
+            .on("mouseover", tip.show);
 
         //Add the line graph
         let lineGraph = d3.line()
-            .x((d) => { return xScale(d["movie_title"]); })
-            .y((d) => { return yScale(d[this.feature]); });
+            .x((d) => { return scaleX(d["movie_title"]); })
+            .y((d) => { return scaleY(d[this.feature]); });
 
         let plotLines = ptg.selectAll(".line")
             .data([this.films]);
