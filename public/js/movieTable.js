b5c448903ca3aa872eb5c774b9cb1c9e18bec253
Sen Thotawatte <sen.thotawatte@gmail.com>
2021-05-13 11:41:18 +0100

    revert last working version

--- public/js/movieTable.js
+++ public/js/movieTable.js
@@ -1,6 +1,6 @@
 class MovieTable
 {
-    constructor(films)
+    constructor(movies)
     {
         //Headers of the table with reference to the CSV file name headers
         this.tableHeaders = ["title_year", "movie_title", "director_name", "imdb_score", "budget"];
@@ -9,7 +9,7 @@ class MovieTable
         this.columnsSortOrder = [ 0, 0, 0, 0, 0];  
 
         //This films reference so constructor can be called when used
-        this.films = films;
+        this.movies = movies;
     }
 
     //Create function to create table for movies
@@ -24,16 +24,16 @@ class MovieTable
 
         //Sorting function added to each column
         columnsTableHead
-            .on("click", (p, q) => {
+            .on("click", (d, i) => {
 
                 
-                if(this.columnsSortOrder[q] % 2 == 0)
+                if(this.columnsSortOrder[i] % 2 == 0)
                 {
                     // For each even click on a column's header, sort all row-values for that column in ascending order
-                    this.films = (this.films).slice().sort(function (x, y) {
-                        if(x[p] < y[p])
+                    this.movies = (this.movies).slice().sort(function (a, b) {
+                        if(a[d] < b[d])
                             return -1;
-                        else if(x[p] > y[p])
+                        else if(a[d] > b[d])
                             return 1;
                         else
                             return 0;
@@ -42,10 +42,10 @@ class MovieTable
                 else    
                 {
                     //Else, for every odd click on a column's header the row-values for that column should be sorted in descending order 
-                    this.films = (this.films).slice().sort(function (x, y) {
-                        if(x[p] > y[p])
+                    this.movies = (this.movies).slice().sort(function (a, b) {
+                        if(a[d] > b[d])
                             return -1;
-                        else if(x[p] < y[p])
+                        else if(a[d] < b[d])
                             return 1;
                         else
                             return 0;
@@ -53,20 +53,20 @@ class MovieTable
                 }
 
                 //Column sort order array updated and apply funtion called to sort data for table with new sort
-                this.columnsSortOrder[q] += 1;
-                this.apply()   
+                this.columnsSortOrder[i] += 1;
+                this.update()   
             });
     }
 
     //Apply function updates the values for the table and displays them
-    apply()
+    update()
     {
         //Setting names of values for body of table from csv file
         let tbody = d3.select("#tableMovies").select("tbody");
 
         //Selecting all values in table body and setting correct values accordingly
         let topRowsBody = tbody.selectAll("tr")
-            .data(this.films);
+            .data(this.movies);
 
         //Appending values from selected values accordingly, removing old ones and mergeing new ones to fit the sorted list
         let topRowsBodyEnter = topRowsBody.enter().append("tr");
@@ -75,9 +75,9 @@ class MovieTable
 
         //Selecting all data from rows and columns and filtering the right data for each of the headers
         let topColumnsBody = topRowsBody.selectAll("td")
-            .data( (p) => {
+            .data( (d) => {
                 return [
-                    p["title_year"], p["movie_title"], p["director_name"], p["imdb_score"], p["budget"]
+                    d["title_year"], d["movie_title"], d["director_name"], d["imdb_score"], d["budget"]
                 ]
             } );
 
@@ -88,14 +88,14 @@ class MovieTable
         //Mergeing this with the new columns after sorting
         topColumnsBody = topColumnsBody.merge(topColumnsBodyEnter)
             .style("opacity", 0)
-            .text( (p, q) => {
-                if(!p)
+            .text( (d, i) => {
+                if(!d)
                     return "N/A";
 
-                if(q == 4)  //Column for budget as some values are 'N/A' (strings)
-                    return parseInt(p).toLocaleString();
+                if(i == 4)  //Column for budget as some values are 'N/A' (strings)
+                    return parseInt(d).toLocaleString();
 
-                return p;
+                return d;
             })
             .transition()
             .duration(500)
