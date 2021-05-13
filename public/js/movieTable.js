8c61b3ca6fc0b4cf89b11eeedb73d88853a5023e
Sen Thotawatte <sen.thotawatte@gmail.com>
2021-05-13 11:18:10 +0100

    changes

--- public/js/movieTable.js
+++ public/js/movieTable.js
@@ -1,6 +1,6 @@
 class MovieTable
 {
-    constructor(movies)
+    constructor(films)
     {
         //Headers of the table with reference to the CSV file name headers
         this.tableHeaders = ["title_year", "movie_title", "director_name", "imdb_score", "budget"];
@@ -9,31 +9,31 @@ class MovieTable
         this.columnsSortOrder = [ 0, 0, 0, 0, 0];  
 
         //This films reference so constructor can be called when used
-        this.movies = movies;
+        this.films = films;
     }
 
     //Create function to create table for movies
     create()
     {
         //Setting name of headers for table head 
-        let thead = d3.select("#tableMovies").select("thead");
+        let tableHead = d3.select("#tableMovies").select("tableHead");
 
         //Selecting all headers in table head and setting the correct data to each column
-        let columnsTableHead = thead.selectAll("th")
+        let columnsTableHead = tableHead.selectAll("th")
             .data(this.tableHeaders);
 
         //Sorting function added to each column
         columnsTableHead
-            .on("click", (d, i) => {
+            .on("click", (p, q) => {
 
                 
-                if(this.columnsSortOrder[i] % 2 == 0)
+                if(this.columnsSortOrder[q] % 2 == 0)
                 {
                     // For each even click on a column's header, sort all row-values for that column in ascending order
-                    this.movies = (this.movies).slice().sort(function (a, b) {
-                        if(a[d] < b[d])
+                    this.films = (this.films).slice().sort(function (x, y) {
+                        if(x[p] < y[p])
                             return -1;
-                        else if(a[d] > b[d])
+                        else if(x[p] > y[p])
                             return 1;
                         else
                             return 0;
@@ -42,10 +42,10 @@ class MovieTable
                 else    
                 {
                     //Else, for every odd click on a column's header the row-values for that column should be sorted in descending order 
-                    this.movies = (this.movies).slice().sort(function (a, b) {
-                        if(a[d] > b[d])
+                    this.films = (this.films).slice().sort(function (x, y) {
+                        if(x[p] > y[p])
                             return -1;
-                        else if(a[d] < b[d])
+                        else if(x[p] < y[p])
                             return 1;
                         else
                             return 0;
@@ -53,20 +53,20 @@ class MovieTable
                 }
 
                 //Column sort order array updated and apply funtion called to sort data for table with new sort
-                this.columnsSortOrder[i] += 1;
-                this.update()   
+                this.columnsSortOrder[q] += 1;
+                this.apply()   
             });
     }
 
     //Apply function updates the values for the table and displays them
-    update()
+    apply()
     {
         //Setting names of values for body of table from csv file
-        let tbody = d3.select("#tableMovies").select("tbody");
+        let topBody = d3.select("#tableMovies").select("topBody");
 
         //Selecting all values in table body and setting correct values accordingly
-        let topRowsBody = tbody.selectAll("tr")
-            .data(this.movies);
+        let topRowsBody = topBody.selectAll("tr")
+            .data(this.films);
 
         //Appending values from selected values accordingly, removing old ones and mergeing new ones to fit the sorted list
         let topRowsBodyEnter = topRowsBody.enter().append("tr");
@@ -75,9 +75,9 @@ class MovieTable
 
         //Selecting all data from rows and columns and filtering the right data for each of the headers
         let topColumnsBody = topRowsBody.selectAll("td")
-            .data( (d) => {
+            .data( (p) => {
                 return [
-                    d["title_year"], d["movie_title"], d["director_name"], d["imdb_score"], d["budget"]
+                    p["title_year"], p["movie_title"], p["director_name"], p["imdb_score"], p["budget"]
                 ]
             } );
 
@@ -88,14 +88,14 @@ class MovieTable
         //Mergeing this with the new columns after sorting
         topColumnsBody = topColumnsBody.merge(topColumnsBodyEnter)
             .style("opacity", 0)
-            .text( (d, i) => {
-                if(!d)
+            .text( (p, q) => {
+                if(!p)
                     return "N/A";
 
-                if(i == 4)  //Column for budget as some values are 'N/A' (strings)
-                    return parseInt(d).toLocaleString();
+                if(q == 4)  //Column for budget as some values are 'N/A' (strings)
+                    return parseInt(p).toLocaleString();
 
-                return d;
+                return p;
             })
             .transition()
             .duration(500)
