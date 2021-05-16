627c61337367e0ec3b5edbc145d816df5bad0f39
Sen Thotawatte <sen.thotawatte@gmail.com>
2021-05-16 15:04:19 +0100

    World cloud completed

--- public/js/wordCloud.js
+++ public/js/wordCloud.js
@@ -2,21 +2,22 @@
  * Created by Sen Thotawatte on 08/05/2021.
  */
 class WordCloud{
-
+    //Constuctor to subset 15 films 
     constructor(films){
         this.films = films.slice(0,15);
 
     }
 
-
+//Update function to update frequency list array 
 update(){
-    let frequency_list = [];
+    let listFrequency = [];
 
-    //check if a word exists
-    function wordExists(word) {
-        return (frequency_list).some(function(elem) {
-            if(elem.text == word){
-                elem.size++;
+    //Function to check if a worst exists 
+    function existingWord(existingWord) {
+        return (listFrequency).some(function(element) {
+            //If the element input texts matches an existing word increase the element size
+            if(element.text == existingWord){
+                element.size++;
                 return true;
             }
             else{
@@ -25,55 +26,58 @@ update(){
         })
     }
 
-        //process data and collect similar words
+        //Processing data and retrieving similar matching words for films
         this.films.forEach((film) => {
-                let plotkeywords = film["plot_keywords"].split("|");
-                plotkeywords.forEach(function (plotword) {
-                    let wordInList = wordExists(plotword.trim())
-                    if(!wordInList){
-                        frequency_list.push({"text": plotword.trim(),"size": 1});
+                //Keywordsplotted variable to store plotted words and separate them
+                let keyWordsPlotted = film["plot_keywords"].split("|");
+
+                //Trimming these words if they match
+                keyWordsPlotted.forEach(function (wordPlotted) {
+                    let listContainingWord = existingWord(wordPlotted.trim())
+                    if(!listContainingWord){
+                        listFrequency.push({"text": wordPlotted.trim(),"size": 1});
                     }
                 })
             });
 
-    console.log(frequency_list)
-
-    //let frequency_list = [{"text":"study","size":40},{"text":"motion","size":15},{"text":"forces","size":10},{"text":"electricity","size":15},{"text":"movement","size":10},{"text":"relation","size":5},{"text":"things","size":10},{"text":"force","size":5},{"text":"ad","size":5},{"text":"energy","size":85},{"text":"living","size":5},{"text":"nonliving","size":5},{"text":"laws","size":15},{"text":"speed","size":45},{"text":"velocity","size":30},{"text":"define","size":5},{"text":"constraints","size":5},{"text":"universe","size":10},{"text":"physics","size":120},{"text":"describing","size":5},{"text":"matter","size":90},{"text":"physics-the","size":5},{"text":"world","size":10},{"text":"works","size":10},{"text":"science","size":70},{"text":"interactions","size":30},{"text":"studies","size":5},{"text":"properties","size":45},{"text":"nature","size":40},{"text":"branch","size":30},{"text":"concerned","size":25},{"text":"source","size":40},{"text":"google","size":10},{"text":"defintions","size":5},{"text":"two","size":15},{"text":"grouped","size":15},{"text":"traditional","size":15},{"text":"fields","size":15},{"text":"acoustics","size":15},{"text":"optics","size":15},{"text":"mechanics","size":20},{"text":"thermodynamics","size":15},{"text":"electromagnetism","size":15},{"text":"modern","size":15},{"text":"extensions","size":15},{"text":"thefreedictionary","size":15},{"text":"interaction","size":15},{"text":"org","size":25},{"text":"answers","size":5},{"text":"natural","size":15},{"text":"objects","size":5},{"text":"treats","size":10},{"text":"acting","size":5},{"text":"department","size":5},{"text":"gravitation","size":5},{"text":"heat","size":10},{"text":"light","size":10},{"text":"magnetism","size":10},{"text":"modify","size":5},{"text":"general","size":10},{"text":"bodies","size":5},{"text":"philosophy","size":5},{"text":"brainyquote","size":5},{"text":"words","size":5},{"text":"ph","size":5},{"text":"html","size":5},{"text":"lrl","size":5},{"text":"zgzmeylfwuy","size":5},{"text":"subject","size":5},{"text":"distinguished","size":5},{"text":"chemistry","size":5},{"text":"biology","size":5},{"text":"includes","size":5},{"text":"radiation","size":5},{"text":"sound","size":5},{"text":"structure","size":5},{"text":"atoms","size":5},{"text":"including","size":10},{"text":"atomic","size":10},{"text":"nuclear","size":10},{"text":"cryogenics","size":10},{"text":"solid-state","size":10},{"text":"particle","size":10},{"text":"plasma","size":10},{"text":"deals","size":5},{"text":"merriam-webster","size":5},{"text":"dictionary","size":10},{"text":"analysis","size":5},{"text":"conducted","size":5},{"text":"order","size":5},{"text":"understand","size":5},{"text":"behaves","size":5},{"text":"en","size":5},{"text":"wikipedia","size":5},{"text":"wiki","size":5},{"text":"physics-","size":5},{"text":"physical","size":5},{"text":"behaviour","size":5},{"text":"collinsdictionary","size":5},{"text":"english","size":5},{"text":"time","size":35},{"text":"distance","size":35},{"text":"wheels","size":5},{"text":"revelations","size":5},{"text":"minute","size":5},{"text":"acceleration","size":20},{"text":"torque","size":5},{"text":"wheel","size":5},{"text":"rotations","size":5},{"text":"resistance","size":5},{"text":"momentum","size":5},{"text":"measure","size":10},{"text":"direction","size":10},{"text":"car","size":5},{"text":"add","size":5},{"text":"traveled","size":5},{"text":"weight","size":5},{"text":"electrical","size":5},{"text":"power","size":5}];
-    //d3.select("#wordcloud").remove();
-
+    //Color variable storing scale color of trend function
     let color = d3v3.scale.linear()
         .domain([0,1,2,3,4,5,6,10,15,20])
         .range(["#ddd", "#ccc", "#bbb", "#aaa", "#999", "#888", "#777", "#666", "#555", "#444", "#333"]);
 
-    d3v3.layout.cloud().size([800, 300])
-        .words(frequency_list)
+    //Cloud size storing the words 
+    d3v3.layout.cloud().size([799, 299])
+        .words(listFrequency)
         .rotate(0)
-        .fontSize(function(d) { return d.size*5; })
+        .fontSize(function(f) { 
+            return d.size*4; 
+        })
         .on("end", draw)
         .start();
 
-    // let wordcanvas = d3.select("#wordcanvas")
-    //     .attr("width", 850)
-    //     .attr("height", 350)
-    //     .attr("class", "wordcloud")
-    function draw(words) {
+    //Draw function to draw word on trend screen 
+    function draw(cloudWords) {
         d3v3.select("#wordcanvas")
-            .attr("width", 850)
-            .attr("height", 350)
             .attr("class", "wordcloud")
+            .attr("height", 350)
+            .attr("width", 850)
             .append("g")
-            // without the transform, words words would get cutoff to the left and top, they would
-            // appear outside of the SVG area
             .attr("transform", "translate(200,200)")
             .selectAll("text")
-            .data(words)
+            .data(cloudWords)
             .enter().append("text")
-            .style("font-size", function(d) { return d.size*3 + "px"; })
-            .style("fill", function(d, i) { return color(i); })
-            .attr("transform", function(d) {
-                return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+            .style("font-size", function(f) { 
+                return f.size*3 + "px"; 
             })
-            .text(function(d) { return d.text; });
+            .style("fill", function(f, q) { 
+                return color(q); 
+            })
+            .attr("transform", function(f) {
+                return "translate(" + [f.x, f.y] + ")rotate(" + f.rotate + ")";
+            })
+            .text(function(f) { 
+                return f.text; 
+            });
     }
 
 
